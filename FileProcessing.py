85ed3899e1073f0db11724d01f8fb59b5b96108a
Vladyslav Hladchenko <hladcvla@fel.cvut.cz>
2017-10-18 07:48:08 +0200

    nf

--- FileProcessing.py
+++ FileProcessing.py
@@ -1,7 +1,12 @@
+import os
+
 from NLPtools import NLPtools
 
 nlp = NLPtools("/home/vlad/Documents/CoreNLP/", "/home/vlad/Documents/stanford-ner-2017-06-09/")
 
-nlp.alterto_crf_trainfile("\n\n","\n","DATA/","sentences.txt")
-nlp.usemodel("sentences.txt","english.all.3class.distsim.crf.ser.gz","blah.txt")
+#nlp.alterto_crf_textfile("\n\n","\n","DATA/","mata.txt","matatest.txt")
+#nlp.usemodel("mata.txt","english.all.3class.distsim.crf.ser.gz","mata_entities.txt")
+#nlp.nerouttocolumns("mata_entities.txt","mata_formated.txt")
 
+with open("/home/vlad/Documents/CoreNLP/mata_entities.txt") as ff:
+    print(    ff.read().split("\n").__len__())
--- NLPtools.py
+++ NLPtools.py
@@ -1,31 +1,46 @@
 
 import os
 import subprocess
-
+from collections import OrderedDict
 
 class NLPtools:
     def __init__(self, files, nlpdir):
         self.files = files
         self.nlpdir = nlpdir
 
-    # attr1: between what the messase in file?
+    # attr1: between what the text in file?
     # return number of processed files
-
-    def alterto_crf_trainfile(self, envi, sep, fpath, out_name):
+    # makes testfile too
+    def alterto_crf_textfile(self, envi, sep, fpath, out_name,testname):
         # think about variants
-        i = 0
-        with open(self.files + out_name, 'wb') as write_file:
+        count = 0
+        with open(self.files + out_name, 'w') as write_file, open(self.files + testname,"w") as wt_f:
 
             for root, dirs, files in os.walk(self.files + fpath):
                 for name in files:
                     if name.endswith(".txt"):
                         file_path = os.path.join(root, name)
-                        with open(file_path, 'rb') as text_file:
-                            i += 1
-                            write_file.write(text_file.read().split(envi.encode())[1])
-                            write_file.write(sep.encode()) #last iteration?
-        return i
+                        with open(file_path, 'r') as text_file, open(file_path[:-3] + "an") as ann:
+
+                            content = text_file.read().split(envi)[1]
+                            count += 1
+                            wt_f.write(ann.read())
+                            write_file.write(content)
+                            write_file.write(sep) # last iteration?
+        return count
 
     def usemodel(self, file, model, script_out):
         f = open(self.files + script_out, "w")
         subprocess.call([self.nlpdir + "ner.sh", model, self.files + file], stdout=f)
+
+    def nerouttocolumns(self,inname,outname):
+        with open(self.files  +  inname,'r') as in_file, open(self.files  + outname, 'w') as out_file:
+            for line in in_file.read().split(' '):
+                if line and not line.endswith('\n'):
+
+                    if line.startswith('\n'):
+                        line = line[1:]     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                    out_file.write(line.split('/')[0] + "\t" + line.split('/')[1] )
+                    out_file.write('\n')
+
