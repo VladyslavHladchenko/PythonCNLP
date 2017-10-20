3494f721bbe62237392930b65948ee6e06ac4361
Vladyslav Hladchenko <hladcvla@fel.cvut.cz>
2017-10-20 16:57:57 +0200

    more code

--- FileProcessing.py
+++ FileProcessing.py
@@ -1,17 +1,36 @@
 import os
 from NLPtools import NLPtools
 
-all = "english.all.3class.distsim.crf.ser.gz"
-conll = "english.conll.4class.distsim.crf.ser.gz"
-muc = "english.muc.7class.distsim.crf.ser.gz"
+# distributed models names
+all_3class = "english.all.3class.distsim.crf.ser.gz"
+conll_4class = "english.conll.4class.distsim.crf.ser.gz"
+muc_7class = "english.muc.7class.distsim.crf.ser.gz"
 
 nlp = NLPtools("/home/vlad/Documents/CoreNLP/", "/home/vlad/Documents/stanford-ner-2017-06-09/")
 
 # collect all sentences to one file and
 # separate sentences from different documents by '\n*' because '…' sign  cause problems
-print( nlp.alterto_crf_textfile("\n\n","\n","MATA/","MA/mata.txt"))
+# print( nlp.alterto_crf_textfile("\n\n","\n","DATA/","data.txt","datatest.txt"))
 
-nlp.usemodel("MA/mata.txt",all,"MA/mata_entities.txt")
+#nlp.usemodel("mata.txt",all,"mata_entities.txt")
 
 # entity/TYPE[ ]... -> entity[TAB]TYPE[\n]
-nlp.nerouttocolumns("MA/mata_entities.txt","MA/mata_formated.txt")
+# nlp.nerouttocolumns("mata_entities.txt","mata_formated.txt")
+
+# nlp.testmodel(all_3class, "new.log", 'Conlltrain/formatted/conll2003.testab_formatted.txt')
+# nlp.testmodel(conll_4class, "new.log", 'Conlltrain/formatted/conll2003.train_formatted.txt')
+
+#with open("/home/vlad/Documents/CoreNLP/datatest.txt") as norm, open("/home/vlad/Documents/CoreNLP/dt.txt", 'w') as fake:
+ #   for line in norm.readlines():
+  #      fake.write(line.split('\t')[0] +"\tNT\t" + line.split('\t')[1])
+
+# nlp.combine_models("dt.txt","combined_models_test.txt")
+# nlp.test_combined_model("mucall.crf.ser.gz", "dt.txt")
+
+# nlp.conlltocolumns('Conlltrain/raw/conll2003.train.txt', 'Conlltrain/formatted/conll2003.train_formatted.txt')
+# nlp.trainmodel()
+
+new = "conll.train.crf.ser.gz"
+
+conllold = "conll.distsim.iob2.crf.ser.gz"
+conllold2 = "conll.closed.iob2.crf.ser.gz"
--- NLPtools.py
+++ NLPtools.py
@@ -11,23 +11,23 @@ class NLPtools:
     # attr1: between what the text in file?
     # return number of processed files
     # makes testfile too
-    def alterto_crf_textfile(self, envi, sep, fpath, out_name,testname):
+    def alterto_crf_textfile(self, envi, sep, fpath, out_name, test_name):
         # think about variants
         count = 0
-        with open(self.files + out_name, 'w') as write_file, open(self.files + testname,"w") as wt_f:
+        with open(self.files + out_name, 'w') as write_file,  open(self.files + test_name,"w") as testfile:
 
             for root, dirs, files in os.walk(self.files + fpath):
                 for name in files:
                     if name.endswith(".txt"):
                         file_path = os.path.join(root, name)
                         with open(file_path, 'r') as text_file, open(file_path[:-3] + "an") as ann:
+
                             content = text_file.read().split(envi)[1]
 
                             if count:
-                                wt_f.write(sep[1:])
                                 write_file.write(sep)
                             count += 1
-                            wt_f.write(ann.read())
+                            testfile.write(ann.read())
                             write_file.write(content)
 
         return count
@@ -36,35 +36,48 @@ class NLPtools:
         f = open(self.files + script_out, "w")
         subprocess.call([self.nlpdir + "ner.sh", model, self.files + file,self.files + "matatest.txt"], stdout=f)
 
-    def testmodel(self, file, model, script_out,test):
-        f = open(self.files + script_out, "w")
-        subprocess.call([self.nlpdir + "nertest.sh", model, self.files + test], stdout=f)
+    def testmodel(self, model, script_out,test,custom = False):
+        script = "nertest.sh"
+        if custom:
+            script = "nertestcustom.sh"
 
-    def nerouttocolumns(self,inname,outname):
+        f = open(self.files + script_out, "w")
+        subprocess.call([self.nlpdir + script, model, self.files + test], stdout=f)
+
+    def trainmodel(self):
+        subprocess.call([self.nlpdir + "nertrain.sh"])
+
+    def conlltocolumns(self,inname,outname):
+        with open(self.files + inname, 'r') as in_file, open(self.files + outname, 'w') as out_file:
+            for line in in_file.readlines():
+                splitted = line.split(' ')
+                if splitted.__len__() == 4:
+                    type = 'O'
+                    if splitted[3].__contains__('I-ORG'):
+                        type = "ORGANIZATION"
+                    elif splitted[3].__contains__('I-PER'):
+                        type = "PERSON"
+
+                    out_file.write(splitted[0] + '\t' + type +'\n')
+
+    def combine_models(self):
+        subprocess.call([self.nlpdir + "nercombine.sh"])
+
+    def test_combined_model(self,model,test):
+        subprocess.call([self.nlpdir + "testcombined.sh", self.nlpdir + "customclassifiers/" + model, self.files + test])
+
+
+    def nerouttocolumns(self, inname, outname):
         with open(self.files  +  inname,'r') as in_file, open(self.files  + outname, 'w') as out_file:
             for line in in_file.read().split(' '):
                 if line and not line.endswith('\n'):
 
                     if line.startswith('\n'):
                         line = line[1:]     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    splitted = line.split('/')
+                    if splitted.__len__() > 2 :
+                        out_file.write('/'.join(splitted[:-1]) + '\t' + splitted[-1])
+                    else:
+                        out_file.write(line.split('/')[0] + "\t" + line.split('/')[1] )
 
-                    out_file.write(line.split('/')[0] + "\t" + line.split('/')[1] )
                     out_file.write('\n')
-
-
-    def binaryClassification(self, testfile, formattedout):
-        count =0
-        with open(self.files + testfile,'r') as test, open(self.files + formattedout,'r') as nerout, open(
-                        self.files +"differencies.txt", 'w') as diff:
-            testentities = test.read().split('\n*')
-            nerentities = nerout.read().split('\n*\tO\n')
-            for test, ner in zip(testentities,nerentities):
-                if test.split('\n').__len__() != ner.split('\n').__len__():
-
-                    count +=1
-                    diff.write('\n\t\t\t\t\tTEST \t\t\t' + str( test.split('\n').__len__() - ner.split('\n').__len__() )+'\n')
-                    diff.write(test)
-                    diff.write('\n\t\t\t\t\tNER\n')
-                    diff.write(ner)
-                    diff.write('\n------------------------------------------------------------------\n')
-        #print(count)
